4d95886f9b8d8e07493fd2cf485afcab44ab3bad
Vladimir Semkin <vnsemkin@gmail.com>
2022-06-25 13:45:21 +0300

    LinkedList vs ArrayList Version 3

--- LinkedVsArray/Main.java
+++ LinkedVsArray/Main.java
@@ -20,21 +20,33 @@ public class Main
         System.out.println("<-----Time_to_Add_First_Element_To_LinkedList_Vs_ArrayList----->");
         System.out.println("LinkedList -> :"+linkedListTest.addElementToStartLinkedList() + " ns"
                 + "\n ArrayList -> :" + arrayListTest.addElementToStartArrayList() + " ns");
-        if (linkedListTest.createLinkedList(size)>ArrayListTest.createArrayList(size))
+        if (linkedListTest.addElementToStartLinkedList()>arrayListTest.addElementToStartArrayList())
             System.out.println(YELLOW + "LinkedList is slower" + RESET);
         else System.out.printf(YELLOW + "LinkedList is faster" + RESET);
         System.out.println("\n<-----Time_to_Add_Last_Element_To_LinkedList_Vs_ArrayList----->");
         System.out.println("LinkedList -> :"+linkedListTest.addElementToEndLinkedList(size) + " ns"
                 + "\n ArrayList -> :" + arrayListTest.addElementToEndArrayList(size) + " ns");
+        if (linkedListTest.addElementToEndLinkedList(size)>arrayListTest.addElementToEndArrayList(size))
+            System.out.println(YELLOW + "LinkedList is slower" + RESET);
+        else System.out.printf(YELLOW + "LinkedList is faster" + RESET);
         System.out.println("<-----Time_to_Add_Element_To_Middle_Of_LinkedList_Vs_ArrayList----->");
         System.out.println("LinkedList -> :"+linkedListTest.addElementToMiddleOfLinkedList(size) + " ns"
                 + "\n ArrayList -> :" + arrayListTest.addElementToMiddleOfArrayList(size) + " ns");
+        if (linkedListTest.addElementToMiddleOfLinkedList(size)>arrayListTest.addElementToMiddleOfArrayList(size))
+        System.out.println(YELLOW + "LinkedList is slower" + RESET);
+        else System.out.printf(YELLOW + "LinkedList is faster" + RESET);
         System.out.println("<-----Time_to_Get_Element_From_Middle_Of_LinkedList_Vs_ArrayList----->");
         System.out.println("LinkedList -> :"+linkedListTest.getElementFromMiddleOfLinkedList(size) + " ns"
                 + "\n ArrayList -> :" + arrayListTest.getElementFromMiddleOfArrayList(size) + " ns");
+        if (linkedListTest.getElementFromMiddleOfLinkedList(size)>arrayListTest.getElementFromMiddleOfArrayList(size))
+            System.out.println(YELLOW + "LinkedList is slower" + RESET);
+        else System.out.printf(YELLOW + "LinkedList is faster" + RESET);
         System.out.println("<-----Time_to_Set_Element_To_Middle_Of_LinkedList_Vs_ArrayList----->");
         System.out.println("LinkedList -> :"+linkedListTest.setElementToMiddleOfLinkedList(size) + " ns"
                 + "\n ArrayList -> :" + arrayListTest.setElementToMiddleOfArrayList(size) + " ns");
+        if (linkedListTest.setElementToMiddleOfLinkedList(size)>arrayListTest.setElementToMiddleOfArrayList(size))
+            System.out.println(YELLOW + "LinkedList is slower" + RESET);
+        else System.out.printf(YELLOW + "LinkedList is faster" + RESET);
     }
 }
 
